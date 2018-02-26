4e86d4820a6e65543526488f2fb86e55f3c67e51
dlsyaim <dlsyaim@gmail.com>
2018-02-26 20:28:33 +0800

    first commit

---
+++ src/MyCompanyName.AbpZeroTemplate.Web/Areas/Mpa/Controllers/BasicdataController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+using Abp.Auditing;
+using Abp.Web.Mvc.Authorization;
+using MyCompanyName.AbpZeroTemplate.Authorization;
+using MyCompanyName.AbpZeroTemplate.Web.Controllers;
+
+namespace MyCompanyName.AbpZeroTemplate.Web.Areas.Mpa.Controllers
+{
+    [AbpMvcAuthorize(AppPermissions.Pages_Administration_Basicdata)]
+    public class BasicdataController:AbpZeroTemplateControllerBase
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
